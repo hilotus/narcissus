bbe418aa8567da1baade4a9151a455da41734478
Luo Wei <luowei---010101@163.com>
2015-06-03 00:45:56 +0800

    update

--- Brocfile.js
+++ Brocfile.js
@@ -20,17 +20,6 @@ var app = new EmberApp();
 // jquery.nicescroll.min.js
 app.import('bower_components/jquery-nicescroll/jquery.nicescroll.min.js');
 
-// pure css
-app.import('bower_components/pure/pure-min.css');
-
-// font awesome
-app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf', {destDir: "fonts"});
-app.import('bower_components/font-awesome/fonts/fontawesome-webfont.eot', {destDir: "fonts"});
-app.import('bower_components/font-awesome/fonts/fontawesome-webfont.svg', {destDir: "fonts"});
-app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff', {destDir: "fonts"});
-app.import('bower_components/font-awesome/fonts/fontawesome.otf', {destDir: "fonts"});
-app.import('bower_components/font-awesome/css/font-awesome.min.css');
-
 // moment
 app.import('bower_components/moment/min/moment-with-locales.min.js');
 // moment-timezone
@@ -42,20 +31,5 @@ app.import('bower_components/pace/themes/blue/pace-theme-flash.css');
 
 // js-md5
 app.import('bower_components/js-md5/js/md5.min.js');
-
-// codemirror
-app.import('bower_components/codemirror/lib/codemirror.js');
-app.import('bower_components/codemirror/addon/fold/foldcode.js');
-app.import('bower_components/codemirror/addon/fold/foldgutter.js');
-app.import('bower_components/codemirror/addon/fold/markdown-fold.js');
-app.import('bower_components/codemirror/mode/markdown/markdown.js');
-app.import('bower_components/codemirror/lib/codemirror.css');
-app.import('bower_components/codemirror/addon/fold/foldgutter.css');
-
-// marked
-app.import('bower_components/marked/lib/marked.js');
-
-// highlightjs
-app.import('bower_components/highlightjs/highlight.pack.js');
 
 module.exports = app.toTree();
